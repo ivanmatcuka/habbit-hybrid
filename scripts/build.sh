974cb4799a4c9f91dee5bb70f858150de57549ba
Ivan Matcuka <ivanmatcuka@protonmail.com>
2026-05-09 10:06:38 +0200

    fix: build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,8 @@
 cd habbit-frontend
 git pull
 # npx cap build ios --xcode-export-method debugging
+npm i
+npm run build
+npx cap sync
 keytool -genkey -v -keystore debug.keystore -storepass android -alias androiddebugkey -keypass android -keyalg RSA -keysize 2048 -validity 10000 -dname "C=US, O=Android, CN=Android Debug"
 npx cap build android --keystorepath ../debug.keystore --keystorepass android --keystorealias androiddebugkey --keystorealiaspass android

c5ceedb48643a951ca352f936307c96a91e8a540
Ivan Matcuka <ivanmatcuka@protonmail.com>
2026-05-09 19:41:43 +0200

    debug: cp

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,16 @@
 cd habbit-hybrid
 
 # Update
-git checkout development
-git pull
+# git checkout development
+# git pull
 
 # Build
-docker compose -f ./docker-compose.yml up -d --build
+# docker compose -f ./docker-compose.yml up -d --build
 
 # Extract output from container
-chmod 755 ./android
-mkdir -p ./android/app/build/outputs
-docker cp android_builder:/usr/src/app/android/app/build/outputs/bundle ./android/app/build/outputs
+# chmod 755 ./android
+# mkdir -p ./android/app/build/outputs
+docker cp android_builder:/usr/src/app/package.json .
 
 # Kill
 docker compose down

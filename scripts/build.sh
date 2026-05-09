14242d82835407556a5b861cbce1786f76d4e731
Ivan Matcuka <ivanmatcuka@protonmail.com>
2026-05-09 20:20:57 +0200

    debug

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,11 +5,12 @@ git checkout development
 git pull
 
 # Build
-docker compose -f ./docker-compose.yml up -d --build
+# docker compose -f ./docker-compose.yml up -d --build
+docker compose -f ./docker-compose.yml up -d
 
 # Extract output from container
-chmod 755 ./android
-mkdir -p ./android/app/build/outputs
+# chmod 755 ./android
+# mkdir -p ./android/app/build/outputs
 docker cp android_builder:/usr/src/app/android/app/build/outputs/bundle ./android/app/build/outputs
 
 # Kill

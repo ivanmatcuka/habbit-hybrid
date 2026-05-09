27dc03021202b244afc20248f395e922d37dd5fb
Ivan Matcuka <ivanmatcuka@protonmail.com>
2026-05-09 19:08:21 +0200

    fix: build extraction

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,15 @@
 cd habbit-hybrid
+
+# Update
 git checkout development
 git pull
+
+# Build
 docker compose -f ./docker-compose.yml up -d --build
-docker cp android_builder:/usr/src/app/android/app/build/outputs/bundle ./android/app/build/outputs/bundle
+
+# Extract output from container
+mkdir -p ./android/app/build/outputs
+docker cp android_builder:/usr/src/app/android/app/build/outputs/bundle ./android/app/build/outputs
+
+# Kill
 docker compose down

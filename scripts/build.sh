7d89e1b1aa2cb4b1a7300db7685cc12dad5080c1
Ivan Matcuka <ivanmatcuka@protonmail.com>
2026-05-10 14:40:15 +0200

    fix: variable added

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
-pwd
+pwd=$(pwd)
 
 # Plug in the web repository
 rm -rf ../habbit-frontend
@@ -6,8 +6,7 @@ git clone https://github.com/ivanmatcuka/habbit-frontend.git ../habbit-frontend
 cd ../habbit-frontend
 npm i
 
-cd ..
-ls -l
+cd ../${pwd}
 
 npm install
 npm run build:development

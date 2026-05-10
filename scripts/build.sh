d614d0945016e16d5842e6184b94ed4c9ab5d992
Ivan Matcuka <ivanmatcuka@protonmail.com>
2026-05-10 14:11:11 +0200

    fix: proper path

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 # Plug in the web repository
-RUN git clone https://github.com/ivanmatcuka/habbit-frontend.git habbit-frontend
+RUN git clone https://github.com/ivanmatcuka/habbit-frontend.git ../habbit-frontend
 RUN cd ../habbit-frontend && npm i && ..
 
 npm install

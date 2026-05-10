1600f0edec73b290693ec348eff9d04a109ea5ec
Ivan Matcuka <ivanmatcuka@protonmail.com>
2026-05-10 15:00:41 +0200

    fix: debug key

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,20 @@ npm i
 
 cd ${pwd}
 
+# Generate debug key
+keytool -genkey -v \
+  -keystore debug.keystore \
+  -storepass android \
+  -alias androiddebugkey \
+  -keypass android \
+  -keyalg RSA \
+  -keysize 2048 \
+  -validity 10000 \
+  -dname "C=US, O=Android, CN=Android Debug"
+
+
 npm install
 npm run build:development
 npx cap sync
 npm run build:android
+

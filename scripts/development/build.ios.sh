d52845458f1921596662472ccf86cff40e4e1910
Ivan Matcuka <ivanmatcuka@protonmail.com>
2026-05-11 11:48:26 +0200

    wip: ios build

---
+++ scripts/development/build.ios.sh
@@ -0,0 +1,18 @@
+pwd=$(pwd)
+
+# Plug in the web repository
+rm -rf ../habbit-frontend
+git clone https://github.com/ivanmatcuka/habbit-frontend.git ../habbit-frontend
+cd ../habbit-frontend
+npm i
+
+cd ${pwd}
+
+npm install
+npm run build:development
+npx cap sync ios
+npm run ios:debug
+
+# mkdir -p ./artifacts
+# cp ./android/app/build/outputs/apk/debug/*.apk ./artifacts/
+
